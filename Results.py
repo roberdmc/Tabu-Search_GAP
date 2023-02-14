4567ddc6ea90da697ebffb84b875a34f83e4ab0a
RoberDMC <35946602+roberdmc@users.noreply.github.com>
2023-02-13 22:20:44 -0300

    Show if neighbor is valid

--- Results.py
+++ Results.py
@@ -1,19 +1,23 @@
-def print_results(best_assignment, best_cost, best_hours, availability, it=0, initial = False, final=False):
+def print_results(best_assignment, best_cost, best_hours, availability, it=0, feasible=False, initial = False, final=False):
     if final:
         print()
         print("---- Tabu Search completed successfully ----")
         print()
         print("--- Best valid result: ---")
-    elif not initial:
+    elif initial:
+        print()
+        print("--- Initial solution: ---")
+    else:
         print()
         print(f"--- Iteration {it}: ---")
-        print("--- Best result: ---")
-    else:
-        print("--- Initial solution: ---")
+        if feasible:
+            print("--- Best neighbor (Valid): ---")
+        else:
+            print("--- Best neighbor (Not valid): ---")
     print("Assignment:")
     for row in best_assignment:
         print("\t\t",row)
-    print(f"Cost:\t\t {best_cost}")
+    print(f"Cost: {best_cost}")
     print(f"Hours used:\t {best_hours}")
     print(f"Availability:\t {availability}")
     print()
